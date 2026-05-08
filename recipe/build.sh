ad140a0e822012f19d25de811fa8c7816e6b6270
UENO, M. <eunosium-1128@live.jp>
2026-05-08 13:14:35 +0000

    Fix syntax error in platform condition for max_isa assignment

--- recipe/build.sh
+++ recipe/build.sh
@@ -6,7 +6,7 @@ export TBBROOT=${PREFIX}
 
 if [[ "${target_platform}" == *"-64" ]] ; then
     max_isa="AVX512"
-elif [[ "${target_platform}" == "osx-arm64" ] || [ "${target_platform}" == "linux-aarch64" ]]; then
+elif [[ "${target_platform}" == "osx-arm64" || "${target_platform}" == "linux-aarch64" ]]; then
     max_isa="NEON"
 fi
 
