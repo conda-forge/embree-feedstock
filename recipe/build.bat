0580a33cb89eeae4a000d49dc576e4859776cca7
UENO, M. <eunosium-1128@live.jp>
2026-05-09 01:12:59 +0900

    Convert embree-feedstock to v1 feedstock (#57)
    * Convert to v1 feedstock and use pixi as conda install tool
    
    * MNT: Re-rendered with conda-smithy 3.61.2 and conda-forge-pinning 2026.05.07.16.24.33
    
    Other tools:
    - conda-build 26.3.0
    - rattler-build 0.64.1
    - rattler-build-conda-compat 1.4.14
    
    * Refactor recipe.yaml for Embree package
    
    Updated recipe.yaml to reflect changes in naming, source URL, and added detailed description.
    
    * Update build scripts and recipe.yaml for improved compatibility and formatting
    
    * Update `max_isa` setting to AVX512
    
    * Fix syntax error in platform condition for max_isa assignment
    
    * Update test requirements to reference embree4 library
    
    * Refactor max_isa assignment logic for platform-specific handling in build.sh

---
+++ recipe/build.bat
@@ -0,0 +1,37 @@
+@echo on
+setlocal EnableDelayedExpansion
+
+:: Specify location of TBB
+set "TBBROOT=%LIBRARY_PREFIX%"
+
+if "%target_platform%"=="win-64" (
+    set "max_isa=AVX512"
+) else if "%target_platform%"=="win-arm64" (
+    set "max_isa=NEON"
+)
+
+:: Configure
+cmake -S . -B build -G Ninja ^
+      %CMAKE_ARGS% ^
+      -DBUILD_SHARED_LIBS=ON ^
+      -DEMBREE_TUTORIALS=OFF ^
+      -DEMBREE_MAX_ISA=%max_isa% ^
+      -DEMBREE_ISPC_SUPPORT=ON
+if %ERRORLEVEL% neq 0 exit /b 1
+
+:: Compile
+cmake --build build --parallel %CPU_COUNT%
+if %ERRORLEVEL% neq 0 exit /b 1
+
+if not "%CONDA_BUILD_SKIP_TESTS%"=="1" (
+    ctest -V --test-dir build --parallel %CPU_COUNT%
+)
+if %ERRORLEVEL% neq 0 exit /b 1
+
+cmake --install build --parallel %CPU_COUNT%
+if %ERRORLEVEL% neq 0 exit /b 1
+
+:: remove tutorial models (which embree installed even when EMBREE_TUTORIALS=off)
+:: this is easier than patching embree's cmake
+rd /s /q %LIBRARY_PREFIX%/bin/models
+if %ERRORLEVEL% neq 0 exit /b 1
